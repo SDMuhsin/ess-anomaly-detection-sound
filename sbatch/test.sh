616ccbfd7b4749ded67892e7534dad3cdce81fdc
sdmuhsin <sdmuhsin@gmail.com>
2025-08-19 09:55:43 -0600

    test sbatch not cuda/11.8

--- sbatch/test.sh
+++ sbatch/test.sh
@@ -8,7 +8,7 @@ sbatch \
     --output=./logs/env-test-%j.out \
     --chdir=/scratch/sdmuhsin/ess-anomaly-detection-sound \
     --wrap="
-        module load gcc arrow cuda/11.8
+        module load gcc arrow cuda
 	nvidia-smi
         source /scratch/sdmuhsin/ess-anomaly-detection-sound/env/bin/activate
         python3 -c 'import torch; print(f\"CUDA available: {torch.cuda.is_available()}\"); print(f\"Device name: {torch.cuda.get_device_name(0)}\")'
